aea4ee4e8b355442784812264fb09e7aeb3a0ec2
Kjeff24 <jeffreyarthur123@gmail.com>
2025-01-26 16:16:05 +0000

    created an image metadata model

---
+++ src/main/java/org/example/model/ImageMetadata.java
@@ -0,0 +1,42 @@
+package org.example.model;
+
+import lombok.*;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSecondaryPartitionKey;
+
+
+@DynamoDbBean
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageMetadata {
+    private String photoId;
+    private String owner;
+    private String imageUrl;
+    private String uploadDate;
+
+    @DynamoDbPartitionKey
+    public String getPhotoId() {
+        return photoId;
+    }
+
+    @DynamoDbAttribute(value = "owner")
+    @DynamoDbSecondaryPartitionKey(indexNames = "OwnerIndex")
+    public String getOwner() {
+        return owner;
+    }
+
+    @DynamoDbAttribute(value = "imageUrl")
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    @DynamoDbAttribute(value = "uploadDate")
+    public String getUploadDate() {
+        return uploadDate;
+    }
+}

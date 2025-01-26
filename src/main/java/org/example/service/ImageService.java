ed844d2fced3b694417d439b01756c13abe3804d
Kjeff24 <jeffreyarthur123@gmail.com>
2025-01-26 16:17:36 +0000

    Get lambda function response and return to api response

--- src/main/java/org/example/service/ImageService.java
+++ src/main/java/org/example/service/ImageService.java
@@ -1,7 +1,8 @@
 package org.example.service;
 
 import org.example.dto.ImageUploadRequest;
+import org.example.model.ImageMetadata;
 
 public interface ImageService {;
-    String uploadImage(ImageUploadRequest imageUploadRequest);
+    ImageMetadata uploadImage(ImageUploadRequest imageUploadRequest);
 }

352104b970b58bdd13698d6a130f670a77fc873a
kjeff24 <jeffreyarthur123@gmail.com>
2025-01-28 13:25:08 +0000

    Created a blog repository

--- src/main/java/org/example/repository/impl/BlogRepositoryImpl.java
+++ src/main/java/org/example/repository/impl/BlogRepositoryImpl.java
@@ -2,16 +2,20 @@ package org.example.repository.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.example.model.BlogPost;
-import org.example.repository.ImageRepository;
+import org.example.repository.BlogRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+
+import java.net.URL;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class ImageRepositoryImpl implements ImageRepository {
+public class BlogRepositoryImpl implements BlogRepository {
     @Value("${aws.dynamodb.table}")
     private String tableName;
 
@@ -26,6 +30,11 @@ public class ImageRepositoryImpl implements ImageRepository {
 
     public void save(BlogPost metadata) {
         getTable().putItem(metadata);
+    }
+
+    public Optional<BlogPost> findByPhotoId(String photoId) {
+        Key key = Key.builder().partitionValue(photoId).build();
+        return Optional.ofNullable(getTable().getItem(r -> r.key(key)));
     }
 
 }

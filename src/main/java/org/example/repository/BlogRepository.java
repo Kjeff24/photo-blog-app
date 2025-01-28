34ab05cb057ded208b898e958b55bcede4f09794
kjeff24 <jeffreyarthur123@gmail.com>
2025-01-28 13:25:08 +0000

    Created a blog repository

--- src/main/java/org/example/repository/BlogRepository.java
+++ src/main/java/org/example/repository/BlogRepository.java
@@ -2,6 +2,11 @@ package org.example.repository;
 
 import org.example.model.BlogPost;
 
-public interface ImageRepository {
+import java.net.URL;
+import java.util.Optional;
+
+public interface BlogRepository {
     void save(BlogPost metadata);
+
+    Optional<BlogPost> findByPhotoId(String photoId);
 }

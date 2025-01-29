cccfd31712c4e285af4be788d1c43a0951823a35
kjeff24 <jeffreyarthur123@gmail.com>
2025-01-29 16:14:45 +0000

    Added method to find photo, find all, find by user email and delete blog post

--- src/main/java/org/example/repository/BlogRepository.java
+++ src/main/java/org/example/repository/BlogRepository.java
@@ -2,11 +2,17 @@ package org.example.repository;
 
 import org.example.model.BlogPost;
 
-import java.net.URL;
+import java.util.List;
 import java.util.Optional;
 
 public interface BlogRepository {
     void save(BlogPost metadata);
 
     Optional<BlogPost> findByPhotoId(String photoId);
+
+    List<BlogPost> findAll();
+
+    List<BlogPost> findAllByUserEmail(String userEmail);
+
+    boolean deleteBlogPost(String photoId, String userEmail);
 }
--- src/main/java/org/example/repository/impl/BlogRepositoryImpl.java
+++ src/main/java/org/example/repository/impl/BlogRepositoryImpl.java
@@ -6,11 +6,16 @@ import org.example.repository.BlogRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbIndex;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.Page;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
 
 import java.net.URL;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -35,6 +40,40 @@ public class BlogRepositoryImpl implements BlogRepository {
     public Optional<BlogPost> findByPhotoId(String photoId) {
         Key key = Key.builder().partitionValue(photoId).build();
         return Optional.ofNullable(getTable().getItem(r -> r.key(key)));
+    }
+
+    public List<BlogPost> findAll() {
+        return getTable().scan().items().stream()
+                .sorted(Comparator.comparing(BlogPost::getUploadDate, Comparator.nullsLast(Comparator.naturalOrder())))
+                .toList();
+    }
+
+    public List<BlogPost> findAllByUserEmail(String userEmail) {
+        DynamoDbIndex<BlogPost> index = getTable().index("OwnerIndex");
+        return index.query(r -> r.queryConditional(
+                        QueryConditional.keyEqualTo(k -> k.partitionValue(userEmail))))
+                .stream()
+                .map(Page::items)
+                .flatMap(List::stream)
+                .sorted(Comparator.comparing(BlogPost::getUploadDate, Comparator.nullsLast(Comparator.naturalOrder())))
+                .toList();
+    }
+
+    public boolean deleteBlogPost(String photoId, String owner) {
+
+        Key key = Key.builder()
+                .partitionValue(photoId)
+                .sortValue(owner)
+                .build();
+
+        Optional<BlogPost> blogPost = findByPhotoId(photoId);
+
+        if (blogPost.isEmpty()) {
+            return false;
+        }
+
+        getTable().deleteItem(key);
+        return true;
     }
 
 }

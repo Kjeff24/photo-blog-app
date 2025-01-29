4a237e225e791c754f0d00a424a9ce3dfdf450f5
kjeff24 <jeffreyarthur123@gmail.com>
2025-01-29 07:34:00 +0000

    Added a lambda function triggered by sqs

--- src/main/java/org/example/SQSEventLambda.java
+++ src/main/java/org/example/SQSEventLambda.java
@@ -2,20 +2,110 @@ package org.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.GetSubscriptionAttributesRequest;
+import software.amazon.awssdk.services.sns.model.ListSubscriptionsByTopicRequest;
+import software.amazon.awssdk.services.sns.model.ListSubscriptionsByTopicResponse;
+import software.amazon.awssdk.services.sns.model.MessageAttributeValue;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.SetSubscriptionAttributesRequest;
+import software.amazon.awssdk.services.sns.model.Subscription;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class NotifyUserLambda implements RequestHandler<Map<String, String>, String> {
-    public NotifyUserLambda() {
+public class SQSEventLambda implements RequestHandler<SQSEvent, Void> {
+    private final SnsClient snsClient;
+    private final String notificationTopicArn;
 
+    public SQSEventLambda() {
+        snsClient = SnsClient.create();
+        notificationTopicArn = System.getenv("NOTIFICATION_TOPIC_ARN");
     }
 
-    public String handleRequest(Map<String, String> event, Context context) {
-        String email = event.get("email");
-        String fullName = event.get("fullName");
+    @Override
+    public Void handleRequest(SQSEvent sqsEvent, Context context) {
+        for (SQSEvent.SQSMessage message : sqsEvent.getRecords()) {
+            String workflowType = message.getMessageAttributes().get("workflowType").getStringValue();
 
-        context.getLogger().log("Email: " + email);
-        context.getLogger().log("FullName: " + fullName);
-        return "";
+            if ("publishSNS".equals(workflowType)) {
+                handlePublishSNS(message, context);
+            } else {
+                context.getLogger().log("Unknown workflowType: " + workflowType);
+            }
+        }
+        return null;
+    }
+
+    private void handlePublishSNS(SQSEvent.SQSMessage message, Context context) {
+        context.getLogger().log("Processing PublishToSNS event: " + message);
+
+        String userEmail = message.getMessageAttributes().get("owner").getStringValue();
+        String subject = message.getMessageAttributes().get("subject").getStringValue();
+        String messageBody = message.getBody();
+
+        sendSNSNotification(userEmail, messageBody, subject);
+    }
+
+    private void sendSNSNotification(String userEmail, String message, String subject) {
+        Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
+
+        if (userEmail != null && !userEmail.isEmpty()) {
+            messageAttributes.put("userEmail", MessageAttributeValue.builder()
+                    .dataType("String")
+                    .stringValue(userEmail)
+                    .build());
+        }
+
+        addSubscriptionFilter(userEmail);
+
+        PublishRequest publishRequest = PublishRequest.builder()
+                .subject(subject)
+                .topicArn(notificationTopicArn)
+                .message(message)
+                .messageAttributes(messageAttributes)
+                .build();
+
+        snsClient.publish(publishRequest);
+    }
+
+    private void addSubscriptionFilter(String userEmail) {
+        ListSubscriptionsByTopicResponse subscriptionsResponse = snsClient.listSubscriptionsByTopic(
+                ListSubscriptionsByTopicRequest.builder()
+                        .topicArn(notificationTopicArn)
+                        .build()
+        );
+
+        for (Subscription subscription : subscriptionsResponse.subscriptions()) {
+            String subscriptionArn = subscription.subscriptionArn();
+
+            if ("PendingConfirmation".equals(subscriptionArn) || "Deleted".equals(subscriptionArn)) {
+                continue;
+            }
+
+            String endpoint = snsClient.getSubscriptionAttributes(
+                    GetSubscriptionAttributesRequest.builder()
+                            .subscriptionArn(subscriptionArn)
+                            .build()
+            ).attributes().get("Endpoint");
+
+            String filterPolicy;
+
+            if (userEmail.equals(endpoint)) {
+                filterPolicy = String.format(String.format("{\"userEmail\": [\"%s\"]}", userEmail));
+            } else {
+                filterPolicy = "{\"userEmail\": [\"none\"]}";
+            }
+
+            snsClient.setSubscriptionAttributes(
+                    SetSubscriptionAttributesRequest.builder()
+                            .subscriptionArn(subscriptionArn)
+                            .attributeName("FilterPolicy")
+                            .attributeValue(filterPolicy)
+                            .build()
+            );
+
+        }
     }
 }

695fc30d307a397b77692a916a08a1f17233e244
Kjeff24 <jeffreyarthur123@gmail.com>
2025-01-30 17:41:48 +0000

    Remove wild card imports from BlogPost

--- src/main/java/org/example/model/BlogPost.java
+++ src/main/java/org/example/model/BlogPost.java
@@ -1,7 +1,15 @@
 package org.example.model;
 
-import lombok.*;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSecondaryPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
 
 
 @DynamoDbBean

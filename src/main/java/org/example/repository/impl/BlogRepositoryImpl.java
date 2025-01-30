7d5bd7a214014f3b4f4eea3afb7071d1b9a9e493
Kjeff24 <jeffreyarthur123@gmail.com>
2025-01-30 16:43:35 +0000

    Querying by primary key to include partition and sort key

--- src/main/java/org/example/repository/impl/BlogRepositoryImpl.java
+++ src/main/java/org/example/repository/impl/BlogRepositoryImpl.java
@@ -5,15 +5,10 @@ import org.example.model.BlogPost;
 import org.example.repository.BlogRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbIndex;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.Key;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.*;
 import software.amazon.awssdk.enhanced.dynamodb.model.Page;
 import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
 
-import java.net.URL;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -37,8 +32,8 @@ public class BlogRepositoryImpl implements BlogRepository {
         getTable().putItem(metadata);
     }
 
-    public Optional<BlogPost> findByPhotoId(String photoId) {
-        Key key = Key.builder().partitionValue(photoId).build();
+    public Optional<BlogPost> findByPhotoId(String photoId, String owner) {
+        Key key = getKey(photoId, owner);
         return Optional.ofNullable(getTable().getItem(r -> r.key(key)));
     }
 
@@ -60,20 +55,21 @@ public class BlogRepositoryImpl implements BlogRepository {
     }
 
     public boolean deleteBlogPost(String photoId, String owner) {
-
-        Key key = Key.builder()
-                .partitionValue(photoId)
-                .sortValue(owner)
-                .build();
-
-        Optional<BlogPost> blogPost = findByPhotoId(photoId);
+        Optional<BlogPost> blogPost = findByPhotoId(photoId, owner);
 
         if (blogPost.isEmpty()) {
             return false;
         }
 
-        getTable().deleteItem(key);
+        getTable().deleteItem(getKey(photoId, owner));
         return true;
+    }
+
+    private Key getKey(String photoId, String owner) {
+        return Key.builder()
+                .partitionValue(photoId)
+                .sortValue(owner)
+                .build();
     }
 
 }

5c2b8be9d2f7abcab71ece7ea6655c739e132676
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-16 10:21:52 +0000

    Return BlogPostResponse and list of BlogPostResponse

--- src/main/java/org/example/controller/HealthController.java
+++ src/main/java/org/example/controller/HealthController.java
@@ -3,13 +3,9 @@ package org.example.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.dto.HealthStatus;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,23 +15,10 @@ public class HealthController {
 
     @Value("${aws.region}")
     private String region;
-    private boolean isHealthy = true; // Default to healthy
 
     @GetMapping
     public ResponseEntity<?> checkHealth() {
-        if (isHealthy) {
-            return ResponseEntity.ok(HealthStatus.builder().status("UP").region(region).build());
-        } else {
-            return ResponseEntity
-                    .status(HttpStatus.SERVICE_UNAVAILABLE)
-                    .body(HealthStatus.builder().status("DOWN").region(region).build());
-        }
+        return ResponseEntity.ok(HealthStatus.builder().status("UP").region(region).build());
     }
 
-    @PostMapping("/toggle/{status}")
-    @ResponseStatus(HttpStatus.OK)
-    public String toggleHealth(@PathVariable("status") boolean status) {
-        isHealthy = status;
-        return "Health status set to: " + (status ? "Healthy" : "Unhealthy");
-    }
 }
--- src/main/java/org/example/exception/CustomBadRequestException.java
+++ src/main/java/org/example/exception/CustomBadRequestException.java
@@ -1,7 +1,9 @@
 package org.example.exception;
 
-public class CustomBadRequestException extends RuntimeException{
-    public CustomBadRequestException(){}
+public class CustomBadRequestException extends RuntimeException {
+    public CustomBadRequestException() {
+    }
+
     public CustomBadRequestException(String message) {
         super(message);
     }
